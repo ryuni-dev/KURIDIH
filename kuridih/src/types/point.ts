027929da2881b63ffb9419e0ceaeabec8d74eb05
ryuni-dev <ryuni.dev@gmail.com>
2023-06-17 15:15:30 +0900

    fix: 불필요한 interface 제거

--- kuridih/src/types/point.ts
+++ kuridih/src/types/point.ts
@@ -2,15 +2,3 @@ export interface Point {
     x: number;
     y: number;
 }
-
-export interface Draw {
-    startPoint: Point;
-    prevPoint: Point,
-    currentPoint: Point;
-    isDraw: boolean;
-    ctx: CanvasRenderingContext2D
-}
-
-export interface Mode {
-    
-}
